ce5b8b6a88f589863e1d4b448bbc906284cd7523
Zouambia Sohaib <js_zouambia@esi.dz>
2021-12-18 00:11:58 +0100

    fixed home page

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="homeContainer">
         <div className="relative h-screen pt-60 Container">
           <div className="w-9/12 mx-auto grid grid-cols-3 my-auto">
-            <a href="./prayer">
+            <a href="/prayer">
               {" "}
               <div className="userType w-7/12 mx-auto py-8 h-full">
                 <img
@@ -24,9 +24,8 @@ const Home = () => {
                 مصلي
                 </div>
               </div>
-            </div>
           </a>
-          <a href='./tourist'>
+          <a href='/tourist'>
             <div className="userType w-7/12 mx-auto py-8  h-full">
               <img
                 src="/imgs/visitor.png"
@@ -36,9 +35,9 @@ const Home = () => {
               <div className="text-center text-white mt-5 cursor-pointer">
                 سائح
               </div>
+              </div>
             </a>
-
-          <a href='/student'>
+            <a href='/student'>
             <div className="userType w-7/12 mx-auto py-8 h-full">
               <img
                 src="/imgs/reading-book.png"
@@ -48,13 +47,14 @@ const Home = () => {
               <div className="text-center text-white mt-5 cursor-pointer">
                 قارئ/باحث
               </div>
+              </div>
             </a>
+          </div>
           </div>
           <a href="/map"><button className="absolute bottom-12 right-12 px-6 py-2 bg-green-800 rounded-md text-white">الخريطة العامة</button></a>
           <Navbar></Navbar>
           <Footer></Footer>
         </div>
-      </div>
     </>
   );
 };
